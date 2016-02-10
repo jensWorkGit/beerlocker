322448891aa0718c4b1eb46e01b709fbabca7189
jhoffmann <jens.hoffmann@lidl.com>
2016-02-10 13:55:56 +0100

    * formatted the code.

--- beerlocker-5/models/user.js
+++ beerlocker-5/models/user.js
@@ -3,15 +3,15 @@ var mongoose = require('mongoose');
 
 // Define our user schema
 var UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 // Export the Mongoose model

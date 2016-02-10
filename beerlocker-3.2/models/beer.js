5d2fb06c6fc70b5fcd055cc864f37d026d4b995b
jhoffmann <jens.hoffmann@lidl.com>
2016-02-10 14:39:12 +0100

    * formatted some files.

--- beerlocker-3.2/models/beer.js
+++ beerlocker-3.2/models/beer.js
@@ -2,11 +2,11 @@
 var mongoose = require('mongoose');
 
 // Define our beer schema
-var BeerSchema   = new mongoose.Schema({
-  name: String,
-  type: String,
-  quantity: Number,
-  userId: String
+var BeerSchema = new mongoose.Schema({
+    name: String,
+    type: String,
+    quantity: Number,
+    userId: String
 });
 
 // Export the Mongoose model
